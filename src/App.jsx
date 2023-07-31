73a58c64868339eecbb5be603d10e753c9cba42a
Ihor <132585512+IhorZolot@users.noreply.github.com>
2023-07-31 16:49:22 +0300

    final final fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import 'modern-normalize'
+ import 'modern-normalize'
 
 import user from './assets/user.json';
-import data from './assets/data.json';
 import friends from './assets/friends.json';
 import transactions from './assets/transactions.json';
+import data from './assets/data.json';
 
 import { Profile } from './components/Profile/Profile';
 import { Container } from './components/Profile/Profile.styled';
 
 import { FriendList } from './components/Friends/FriendList';
 import {TransactionHistory} from  './components/Ttansactions/TransactionHistory';
-import { Statistics } from 'components/Statistic/Statistics';
+import { Statistics } from './components/Statistics/Statistics';
 
 
 export const App = () => {
   return (
     <Container>
      <Profile {...user}/>
-     <Statistics title="Upload stats" stats={data} />
-     <FriendList friends={friends} />;
+     <FriendList friends={friends} />
+     <Statistics title='upload stat' stats={data}/>
      <TransactionHistory items={transactions} />
     </Container>
   );
